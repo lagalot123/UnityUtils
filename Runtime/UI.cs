eb4f7fcf0aec4b206e40d8d5543ca21c7adee2d4
wasd <75327586+lagalot123@users.noreply.github.com>
2024-04-02 12:36:09 +0200

    wasd

--- Runtime/UI.cs
+++ Runtime/UI.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 namespace UnityUtils.Runtime {
     public static class UI {
 
+        public static T InstantiateUIPrefab<T>(GameObject prefab) where T : Component {
+            return InstantiateUIPrefab<T>(prefab, prefab.transform.parent);
+        }
         public static T InstantiateUIPrefab<T>(GameObject prefab, Transform parent, bool resetPosition = false) where T : Component{
             return InstantiateUIPrefab(prefab, parent, resetPosition).GetComponent<T>();
         }
 
+
+        public static GameObject InstantiateUIPrefab(GameObject prefab) {
+            return InstantiateUIPrefab(prefab, prefab.transform.parent);
+        }
         public static GameObject InstantiateUIPrefab(GameObject prefab, Transform parent, bool resetPosition = false) {
             GameObject tmp = InstantiateUIPrefab(prefab);
 
             tmp.SetActive(true);
+            Debug.Log(tmp.activeSelf);
 
             tmp.transform.SetParent(parent);
             tmp.transform.localScale = prefab.transform.localScale;
@@ -23,10 +31,6 @@ namespace UnityUtils.Runtime {
             }
 
             return tmp;
-        }
-
-        public static GameObject InstantiateUIPrefab(GameObject prefab) {
-            return Object.Instantiate(prefab, prefab.transform.parent);
         }
 
     }
