060c6d373afa97864bc052e46ee008d48c1e59de
Lagalot <75327586+lagalot123@users.noreply.github.com>
2025-07-17 08:10:16 +0200

    0.0.45
    -take screenshot, added 2-4x resolution options

--- Editor/TakeScreenshot.cs
+++ Editor/TakeScreenshot.cs
@@ -6,18 +6,32 @@ using UnityEngine;
 namespace UnityUtils.Editor {
     public class TakeScreenshot {
 
-        [MenuItem("Tools/TakeScreenshot %t")]
+        [MenuItem("Tools/Sceenshots/TakeScreenshot %t")]
         private static void Save() {
-            Screenshot();
+            Screenshot(1);
         }
-		static void Screenshot() {
+        
+        [MenuItem("Tools/Sceenshots/TakeScreenshot 2x")]
+        private static void Save2() {
+            Screenshot(2);
+        }
+        [MenuItem("Tools/Sceenshots/TakeScreenshot 3x")]
+        private static void Save3() {
+            Screenshot(3);
+        }
+        [MenuItem("Tools/Sceenshots/TakeScreenshot 4x")]
+        private static void Save4() {
+            Screenshot(4);
+        }
+
+		static void Screenshot(int res) {
 
             if (!Directory.Exists(Folder())) {
                 Directory.CreateDirectory(Folder());
             }
 
-            string filename = ScreenShotName();
-			ScreenCapture.CaptureScreenshot(filename, 1);
+            string filename = ScreenShotName(res);
+			ScreenCapture.CaptureScreenshot(filename, res);
 		}
 
         public static string Folder() {
@@ -25,11 +39,11 @@ namespace UnityUtils.Editor {
                                  Application.dataPath);
         }
 
-		public static string ScreenShotName() {
+		public static string ScreenShotName(int res) {
             return string.Format("{0}/screen_{1}x{2}_{3}.png",
                                  Folder(),
-                                 Screen.width,
-                                 Screen.height,
+                                 Screen.width * res,
+                                 Screen.height * res,
                                  System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
         }
     }
